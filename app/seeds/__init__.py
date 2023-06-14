f0ba038c48f1fb4cecf07c433496973a453c2cb1
CaSandraSmith <casresm3@gmail.com>
2023-06-14 06:18:29 -0400

    Album seeders complete

--- app/seeds/__init__.py
+++ app/seeds/__init__.py
@@ -1,6 +1,7 @@
 from flask.cli import AppGroup
 from .users import seed_users, undo_users
 from .artists import seed_artists, undo_artists
+from .albums import seed_albums, undo_albums
 
 from app.models.db import db, environment, SCHEMA
 
@@ -17,16 +18,19 @@ def seed():
         # command, which will  truncate all tables prefixed with 
         # the schema name (see comment in users.py undo_users function).
         # Make sure to add all your other model's undo functions below
+        undo_albums()
         undo_artists()
         undo_users()
     seed_users()
     seed_artists()
+    seed_albums()
     # Add other seed functions here
 
 
 # Creates the `flask seed undo` command
 @seed_commands.command('undo')
 def undo():
+    undo_albums()
     undo_artists()
     undo_users()
     # Add other undo functions here
--- app/seeds/albums.py
+++ app/seeds/albums.py
@@ -1,8 +1,32 @@
-from app.models import Artist, db, environment, SCHEMA
+from app.models import Album, db, environment, SCHEMA
 from sqlalchemy.sql import text
 
 def seed_albums():
-
+    album1 = Album (
+        name="album1",
+        artist_id=1
+    )
+    album2 = Album (
+        name="album1",
+        artist_id=2
+    )
+    album3 = Album (
+        name="album1",
+        artist_id=3
+    )
+    album4 = Album (
+        name="album1",
+        artist_id=4
+    )
+    album5 = Album (
+        name="album1",
+        artist_id=5
+    )
+    db.session.add(album1)
+    db.session.add(album2)
+    db.session.add(album3)
+    db.session.add(album4)
+    db.session.add(album5)
     db.session.commit()
 
 
