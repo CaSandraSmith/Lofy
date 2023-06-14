92aeafcec532da8912287864d63e3082cb24be52
CaSandraSmith <casresm3@gmail.com>
2023-06-14 12:00:05 -0400

    Added some styling to splash page

---
+++ react-app/src/components/SplashPage/index.js
@@ -0,0 +1,17 @@
+import "./SplashPage.css"
+
+export default function SplashPage() {
+    return (
+        <div className="whole-splash-page">
+            <div className="circle-one"></div>
+            <div className="circle-two"></div>
+            <div className="circle-three"></div>
+            <div className="slash-page-text">
+                <h1>Relax or get focused to music free of distractions</h1>
+                <button>
+                    <h2>GET LOFY FREE</h2>
+                </button>
+            </div>
+        </div>
+    )
+}
