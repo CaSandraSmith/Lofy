b9c42455921371c5a97909347ed14f59797ea55c
CaSandraSmith <casresm3@gmail.com>
2023-06-17 10:25:13 -0400

    added images to album seeds

--- app/seeds/albums.py
+++ app/seeds/albums.py
@@ -4,27 +4,27 @@ from sqlalchemy.sql import text
 def seed_albums():
     album1 = Album (
         name="album1",
-        cover_image="google.com",
+        cover_image="https://lofy.s3.us-east-2.amazonaws.com/album_covers/modern-pop-lofi-music-cover-art-design-template-f6db8ab55ee30bac80c523b3129a268d_screen.jpg",
         artist_id=1
     )
     album2 = Album (
         name="album2",
-        cover_image="google.com",
+        cover_image="https://lofy.s3.us-east-2.amazonaws.com/album_covers/tumblr_p7b18oVqP01ravz9xo1_1280.png",
         artist_id=2
     )
     album3 = Album (
         name="album3",
-        cover_image="google.com",
+        cover_image="https://lofy.s3.us-east-2.amazonaws.com/album_covers/modern-pop-lofi-music-cover-art-design-template-f6db8ab55ee30bac80c523b3129a268d_screen.jpg",
         artist_id=3
     )
     album4 = Album (
         name="album4",
-        cover_image="google.com",
+        cover_image="https://lofy.s3.us-east-2.amazonaws.com/album_covers/tumblr_p7b18oVqP01ravz9xo1_1280.png",
         artist_id=4
     )
     album5 = Album (
         name="album5",
-        cover_image="google.com",
+        cover_image="https://lofy.s3.us-east-2.amazonaws.com/album_covers/tumblr_p7b18oVqP01ravz9xo1_1280.png",
         artist_id=5
     )
     db.session.add(album1)
