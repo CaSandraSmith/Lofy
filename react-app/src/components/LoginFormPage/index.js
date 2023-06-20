bae2be46907f946f97f85ac27bc613fde9258da7
CaSandraSmith <casresm3@gmail.com>
2023-06-20 03:48:22 -0400

    Formatted login page

--- react-app/src/components/LoginFormPage/index.js
+++ react-app/src/components/LoginFormPage/index.js
@@ -21,36 +21,63 @@ function LoginFormPage() {
     }
   };
 
+  const handleDemoClick = async(e) => {
+    e.preventDefault();
+    const data = await dispatch(login("demo@aa.io", "password"));
+    if (data) {
+      setErrors(data);
+    }
+  }
+
   return (
-    <>
-      <h1>Log In</h1>
+    <div className="login-all-content">
+      <div className="log-in-header">
+        <div>
+          <i className="fa-regular fa-face-smile"></i>
+          <h2>Lofy</h2>
+        </div>
+        <h1>Log in to Lofy</h1>
+      </div>
       <form onSubmit={handleSubmit}>
         <ul>
           {errors.map((error, idx) => (
             <li key={idx}>{error}</li>
           ))}
         </ul>
-        <label>
-          Email
-          <input
-            type="text"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            required
-          />
+        <label className="login-input-wrapper">
+          <p>Email</p>
+          <div>
+            <input
+              type="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
+              placeholder="Email"
+            />
+          </div>
         </label>
-        <label>
-          Password
-          <input
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            required
-          />
+        <label className="login-input-wrapper">
+          <p>Password</p>
+          <div>
+            <input
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+              placeholder="Password"
+            />
+          </div>
         </label>
-        <button type="submit">Log In</button>
+        <div className="login-buttons">
+          <button className="login-button" type="submit">Log In</button>
+          <button onClick={handleDemoClick} type="button" className="demo-login-button">Log in as Demo</button>
+        </div>
+        <div className="sign-up-link-on-login">
+          <p>Don't have an account</p>
+          <a href="/signup">Sign up for Spotify</a>
+        </div>
       </form>
-    </>
+    </div>
   );
 }
 
