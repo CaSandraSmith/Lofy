343619c3c056291f33866be0c74e0ab74dfcd342
CaSandraSmith <casresm3@gmail.com>
2023-06-20 05:04:20 -0400

    Gave user logout functionality on all pages

--- react-app/src/components/NavigationBar/GeneralNav/index.js
+++ react-app/src/components/NavigationBar/GeneralNav/index.js
@@ -1,18 +1,36 @@
-import { useSelector } from "react-redux"
+import { useSelector, useDispatch } from "react-redux"
+import { logout } from '../../../store/session';
 import "./GeneralNav.css"
+import { useState } from "react";
+import { useHistory } from "react-router-dom";
 
 export default function GeneralNav() {
+    const dispatch = useDispatch()
+    const history = useHistory()
     let user = useSelector(state => state.session.user)
+	const [menu, setMenu] = useState(false)
+
+    let handleLogout = async () => {
+        history.push("/")
+		dispatch(logout())
+	}
+
     return (
         <div className="general-nav">
-            {user && !user.profile_image ? 
-            <i className="fa-regular fa-user"></i>
-            : user && user.profile_image ?
-            // <img src="" />
-            "Profile" :
-            <div>
-                <button>Sign up</button>
-                <button>Log in</button>
+            {!user.profile_image ?
+                <i 
+                onClick={() => setMenu(!menu)}
+                className="general-nav-profile-no-image fa-regular fa-user"></i>
+                :
+                <img 
+                onClick={() => setMenu(!menu)}
+                className="general-nav-profile-image"
+                src={user.profile_image} />
+            }
+            {menu && 
+            <div className='navbar-general-profile-options'>
+                <div onClick={() => alert("Feature coming soon")}>Account</div>
+                <div onClick={handleLogout}>Log out</div>
             </div>
             }
         </div>
