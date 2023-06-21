dd11b156134ea2a2aef330d50a78b151818a10e5
CaSandraSmith <casresm3@gmail.com>
2023-06-21 19:54:37 -0400

    Fixed navigation bar on general nav so that the user options close when anywhere outside of the menu is clicked

--- react-app/src/components/NavigationBar/GeneralNav/index.js
+++ react-app/src/components/NavigationBar/GeneralNav/index.js
@@ -1,38 +1,55 @@
 import { useSelector, useDispatch } from "react-redux"
 import { logout } from '../../../store/session';
 import "./GeneralNav.css"
-import { useState } from "react";
+import { useState, useRef, useEffect } from "react";
 import { useHistory } from "react-router-dom";
 
 export default function GeneralNav() {
     const dispatch = useDispatch()
     const history = useHistory()
     let user = useSelector(state => state.session.user)
-	const [menu, setMenu] = useState(false)
+    const [menu, setMenu] = useState(false)
+    const profileRef = useRef()
+
+    useEffect(() => {
+        if (!menu) return;
+
+        const closeMenu = (e) => {
+            if (!profileRef.current.contains(e.target)) {
+                setMenu(false);
+            }
+        };
+
+        document.addEventListener("click", closeMenu);
+
+        return () => document.removeEventListener("click", closeMenu);
+    }, [menu]);
 
     let handleLogout = async () => {
         history.push("/")
-		dispatch(logout())
-	}
+        dispatch(logout())
+    }
 
     return (
-        <div className="general-nav">
+        <div className="general-nav" >
             {!user.profile_image ?
-                <i 
-                onClick={() => setMenu(!menu)}
-                className="general-nav-profile-no-image fa-regular fa-user"></i>
+                <i
+                    onClick={() => setMenu(!menu)}
+                    className="general-nav-profile-no-image fa-regular fa-user"></i>
                 :
-                <img 
-                onClick={() => setMenu(!menu)}
-                className="general-nav-profile-image"
-                src={user.profile_image} />
+                <img
+                    onClick={() => setMenu(!menu)}
+                    className="general-nav-profile-image"
+                    src={user.profile_image} />
             }
-            {menu && 
-            <div className='navbar-general-profile-options'>
-                <div onClick={() => alert("Feature coming soon")}>Account</div>
-                <div onClick={handleLogout}>Log out</div>
+            <div ref={profileRef}>
+                {menu &&
+                    <div className='navbar-general-profile-options'>
+                        <div onClick={() => alert("Feature coming soon")}>Account</div>
+                        <div onClick={handleLogout}>Log out</div>
+                    </div>
+                }
             </div>
-            }
         </div>
     )
 }
