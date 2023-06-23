872ae3510ce2f53bb3bd66419e7550661ce634c1
CaSandraSmith <casresm3@gmail.com>
2023-06-23 04:43:55 -0400

    Added about links to home page

--- react-app/src/components/HomePage/SplashPage/SplashFooter.js
+++ react-app/src/components/HomePage/SplashPage/SplashFooter.js
@@ -1,9 +1,22 @@
 import "./SplashPage.css"
+import { Link } from "react-router-dom"
 
 export default function SplashFooter() {
     return (
         <div className="splash-footer">
-            <h1>About Links</h1>
+            <h1>Meet the dev</h1>
+            <div className="about-links">
+                <div>
+                    <a target="_blank" href="https://www.linkedin.com/in/casandra-smith/">
+                        Linkedin <i className="fa-brands fa-linkedin"></i>
+                    </a>
+                </div>
+                <div>
+                    <a target="_blank" href="https://github.com/CaSandraSmith">
+                        <i className="fa-brands fa-github"></i> GitHub
+                    </a>
+                </div>
+            </div>
         </div>
     )
 }
