823c31aa37c47234592cf195498ba6861ccf18c5
CaSandraSmith <casresm3@gmail.com>
2023-07-04 12:05:19 -0400

    Built actions to get all of a users saved songs

--- react-app/src/store/albums.js
+++ react-app/src/store/albums.js
@@ -1,5 +1,6 @@
 const GET_ALL_ALBUMS = "get/allAlbums"
 const GET_ALL_SONGS = "get/allSongs"
+const GET_USER_SAVED_SONGS = "get/savedSongs"
 
 const getAllAlbums = (albums) => ({
     type: GET_ALL_ALBUMS,
@@ -8,6 +9,11 @@ const getAllAlbums = (albums) => ({
 
 const getAllSongs = (songs) => ({
     type: GET_ALL_SONGS,
+    songs
+})
+
+const getSavedSongs = (songs) => ({
+    type: GET_USER_SAVED_SONGS,
     songs
 })
 
@@ -35,14 +41,29 @@ export const gatherAllSongs = () => async(dispatch) => {
     }
 }
 
-let initialState = {albums: {}, songs:{}}
+export const getAllUserSavedSongs = () => async(dispatch) => {
+    let res = await fetch("/api/misc/songs/current_user")
+
+    if (res.ok) {
+        let songs = await res.json()
+        dispatch(getSavedSongs(songs))
+        return songs
+    } else {
+        let errors = await res.json()
+        return errors
+    }
+}
+
+let initialState = {albums: {}, songs:{}, savedSongs : {}}
 
 export default function miscReducer(state = initialState, action) {
     switch (action.type) {
+        case GET_USER_SAVED_SONGS:
+            return {...state, songs:{...action.songs}, albums: {...state.albums}, savedSongs: action.songs}
         case GET_ALL_SONGS:
-            return {...state, songs:{...action.songs}, albums: {...state.albums}}
+            return {...state, songs:{...action.songs}, albums: {...state.albums}, savedSongs: {...state.savedSongs}}
         case GET_ALL_ALBUMS:
-            return {...state, songs:{...state.songs}, albums: {...action.albums}}
+            return {...state, songs:{...state.songs}, albums: {...action.albums}, savedSongs: {...state.savedSongs}}
         default:
             return state
     }
