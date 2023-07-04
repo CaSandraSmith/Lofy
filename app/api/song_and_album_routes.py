2bf95eb0eccbdd9abae17fa445face260b82c08f
CaSandraSmith <casresm3@gmail.com>
2023-07-04 11:57:47 -0400

    Built backend route to handle getting all of a users saved songs

--- app/api/song_and_album_routes.py
+++ app/api/song_and_album_routes.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, jsonify, session, request
 from flask_login import current_user, login_user, logout_user, login_required
-from ..models import Song, Album
+from ..models import Song, Album, User
 
 song_and_album_routes = Blueprint('song_and_album', __name__)
 
@@ -13,6 +13,17 @@ def all_songs_and_albums():
         all_albums[album.id] = album.to_dict()
     
     return all_albums
+
+@song_and_album_routes.route("/songs/current_user")
+@login_required
+def all_songs():
+    user = User.query.get(current_user.id)
+
+    all_songs = {}
+    for song in user.saved_songs:
+        all_songs[song.id] = song.to_dict()
+
+    return all_songs
 
 @song_and_album_routes.route("/songs")
 def all_songs():
