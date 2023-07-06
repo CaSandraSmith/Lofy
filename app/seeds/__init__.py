3e5416f25b227b0bce684b161756233fddfaf600
CaSandraSmith <casresm3@gmail.com>
2023-07-06 17:26:59 -0400

    Connected users saved playlist seeds to main seeding function

--- app/seeds/__init__.py
+++ app/seeds/__init__.py
@@ -8,6 +8,7 @@ from .playlist_songs import seed_playlist_songs, undo_playlist_songs
 from .playlist_reviews import seed_reviews, undo_reviews
 from .user_saved_songs import seed_user_saved_songs, undo_user_saved_songs
 from .user_saved_albums import seed_user_saved_albums, undo_user_saved_albums
+from .user_saved_playlists import seed_user_saved_playlists, undo_user_saved_playlists
 
 from app.models.db import db, environment, SCHEMA
 
@@ -24,6 +25,7 @@ def seed():
         # command, which will  truncate all tables prefixed with 
         # the schema name (see comment in users.py undo_users function).
         # Make sure to add all your other model's undo functions below
+        undo_user_saved_playlists()
         undo_user_saved_albums()
         undo_user_saved_songs()
         undo_reviews()
@@ -42,12 +44,14 @@ def seed():
     seed_reviews()
     seed_user_saved_songs()
     seed_user_saved_albums()
+    seed_user_saved_playlists()
     # Add other seed functions here
 
 
 # Creates the `flask seed undo` command
 @seed_commands.command('undo')
 def undo():
+    undo_user_saved_playlists()
     undo_user_saved_albums()
     undo_user_saved_songs()
     undo_reviews()
