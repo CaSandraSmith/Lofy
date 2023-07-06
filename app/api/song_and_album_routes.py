615185e55c8d8d89c359d51b463e63f8b0540229
CaSandraSmith <casresm3@gmail.com>
2023-07-05 20:18:13 -0400

    Created backend endpoint to handle a user removing a song from their saved albums:

--- app/api/song_and_album_routes.py
+++ app/api/song_and_album_routes.py
@@ -71,15 +71,33 @@ def remove_saved_album(id):
     album = Album.query.get(id)
 
     if not album:
-        return {"errors": "You can't remove a album from your saved albums that doesn't exist"}
+        return {"errors": "You can't remove an album from your saved albums that doesn't exist"}
     
     if album not in user.saved_albums:
-        return {"errors": "You can't remove a album that you haven't saved"}
+        return {"errors": "You can't remove an album that you haven't saved"}
     
     user.saved_albums.remove(album)
     db.session.commit()
 
     return {"message": "Successfully removed from saved albums"}
+
+# add a song to a users saved song
+@song_and_album_routes.route("/albums/current_user/<int:id>", methods=["POST"])
+@login_required
+def add_saved_album(id):
+    user = User.query.get(current_user.id)
+    album = Album.query.get(id)
+
+    if not album:
+        return {"errors": "You can't add an album to your liked albums that doesn't exist"}
+    
+    if album in user.saved_albums:
+        return {"errors": "You can't like a album that you've already liked"}
+    
+    user.saved_albums.append(album)
+    db.session.commit()
+
+    return album.to_dict()
 
 # get all saved albums of current user
 @song_and_album_routes.route("/albums/current_user")
