ba8ef86d7b784816f9052b9214ceeca2de259311
CaSandraSmith <casresm3@gmail.com>
2023-07-07 22:31:52 -0400

    Added buttons to filter what is visible in user library

--- react-app/src/components/ConstantUserNav/LibraryBar/index.js
+++ react-app/src/components/ConstantUserNav/LibraryBar/index.js
@@ -10,6 +10,7 @@ export default function LibraryBar() {
     const history = useHistory()
     const [homeHover, setHomeHover] = useState(false)
     const [searchHover, setSearchHover] = useState(false)
+    const [viewableCategories, setViewableCategories] = useState("all")
 
     useEffect(() => {
         dispatch(getAllUserSavedSongs())
@@ -40,22 +41,22 @@ export default function LibraryBar() {
     return (
         <div className="full-user-nav">
             <div className="library-home-and-search">
-                <div 
-                className="library-general-button" 
-                onClick={() => history.push("/home")}
-                onMouseEnter={() => setHomeHover(true)}
-                onMouseLeave={() => setHomeHover(false)}
+                <div
+                    className="library-general-button"
+                    onClick={() => history.push("/home")}
+                    onMouseEnter={() => setHomeHover(true)}
+                    onMouseLeave={() => setHomeHover(false)}
                 >
-                    <i style={{color: homeHover ? "white" : "#b3b3b3"}} className="fa-solid fa-house"></i>
-                    <p style={{color: homeHover ? "white" : "#b3b3b3"}} >Home</p>
+                    <i style={{ color: homeHover ? "white" : "#b3b3b3" }} className="fa-solid fa-house"></i>
+                    <p style={{ color: homeHover ? "white" : "#b3b3b3" }} >Home</p>
                 </div>
-                <div 
-                className="library-general-button"
-                onMouseEnter={() => setSearchHover(true)}
-                onMouseLeave={() => setSearchHover(false)}
+                <div
+                    className="library-general-button"
+                    onMouseEnter={() => setSearchHover(true)}
+                    onMouseLeave={() => setSearchHover(false)}
                 >
-                    <i style={{color: searchHover ? "white" : "#b3b3b3"}} class="fa-solid fa-magnifying-glass"></i>
-                    <p style={{color: searchHover ? "white" : "#b3b3b3"}} >Search</p>
+                    <i style={{ color: searchHover ? "white" : "#b3b3b3" }} class="fa-solid fa-magnifying-glass"></i>
+                    <p style={{ color: searchHover ? "white" : "#b3b3b3" }} >Search</p>
                 </div>
             </div>
             <div className="library-all-user-playlists">
@@ -68,7 +69,32 @@ export default function LibraryBar() {
                         <i className="fa-solid fa-plus" onClick={handleNewPlaylistClick}></i>
                     </div>
                 </div>
+                <div className="library-options-buttons-wrapper">
+                    {viewableCategories != "all" ?
+                        <button className="library-options-clear" onClick={() => setViewableCategories("all")}><i className="fa-solid fa-x "></i></button>
+                        : null
+                    }
+                    {viewableCategories === "all" || viewableCategories === "playlists" ?
+                        <button className="library-options-buttons" onClick={() => setViewableCategories("playlists")}>Playlists</button>
+                        : null
+                    }
+                    {viewableCategories === "playlists" ?
+                        <button className="library-options-buttons" onClick={() => setViewableCategories("by you")}>By you</button>
+                        : null
+                    }
+                    {viewableCategories === "by you" ? 
+                    <div className="by-you-playlists-button">
+                        <button>Playlists</button>
+                        <button>By you</button>
+                    </div>                        
+                        : null}
+                    {viewableCategories === "all" || viewableCategories === "albums" ?
+                        <button className="library-options-buttons" onClick={() => setViewableCategories("albums")} >Albums</button>
+                        : null
+                    }
+                </div>
                 <div className="current-user-playlists-wrapper">
+                    {viewableCategories === "all" || viewableCategories === "playlists" ?
                     <div className="user-playlists-info" onClick={() => history.push("/collection/tracks")}>
                         <img
                             className="user-playlists-cover-image"
@@ -83,8 +109,8 @@ export default function LibraryBar() {
                                 <p className="library-playlist-info-desc">{formatLikedSongs(savedSongsArr)}</p>
                             </div>
                         </div>
-                    </div>
-                    {playlistArr.length ?
+                    </div> : null}
+                    {playlistArr.length && (viewableCategories === "all" || viewableCategories === "playlists" || viewableCategories === "by you")  ?
                         playlistArr.map(playlist => (
                             <div className="user-playlists-info" onClick={() => history.push(`/playlist/${playlist.id}`)}>
                                 <img className="user-playlists-cover-image"
@@ -102,7 +128,7 @@ export default function LibraryBar() {
                         ))
                         : null
                     }
-                    {savedPlaylistsArr.length ?
+                    {savedPlaylistsArr.length && (viewableCategories === "all" || viewableCategories === "playlists") ?
                         savedPlaylistsArr.map(playlist => (
                             <div className="user-playlists-info" onClick={() => history.push(`/playlist/${playlist.id}`)}>
                                 <img className="user-playlists-cover-image"
@@ -120,7 +146,7 @@ export default function LibraryBar() {
                         ))
                         : null
                     }
-                    {savedAlbumsArr.length ?
+                    {savedAlbumsArr.length && (viewableCategories === "all" || viewableCategories === "albums") ?
                         savedAlbumsArr.map(album => (
                             <div className="user-playlists-info" onClick={() => history.push(`/album/${album.id}`)}>
                                 <img className="user-playlists-cover-image"
