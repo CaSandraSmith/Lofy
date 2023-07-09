c187b9720a87b976bc0ae657215d9f0e19cfa405
CaSandraSmith <casresm3@gmail.com>
2023-07-09 19:55:50 -0400

    Made backend route to follow a user

--- app/api/user_routes.py
+++ app/api/user_routes.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, jsonify
 from flask_login import login_required
-from app.models import User
+from app.models import User, db
 
 user_routes = Blueprint('users', __name__)
 
@@ -23,3 +23,23 @@ def user(id):
     """
     user = User.query.get(id)
     return user.to_dict()
+
+
+@user_routes.route('/follow/<int:id>', methods=["POST"])
+@login_required
+def follow_user(id):
+    """
+    Gets the user that the current user wants to follow and adds them to their followers. Returns the user who was followed.
+    """
+    current_user = User.query.get(current_user.id)
+    user = User.query.get(id)
+
+    if not user:
+        return {"errors": "User couldn't be found"}
+    
+    if user in current_user.following:
+        return {"errors" : "You can't follow a user that you already follow"}
+    
+    current_user.following.append(user)
+
+    return current_user.to_dict()
