670b70aea92c1656bcf48b6a05dbab08be6dc6df
CaSandraSmith <casresm3@gmail.com>
2023-07-10 04:35:53 -0400

    Added some styling to current user profile

--- react-app/src/components/UserProfiles/CurrentUserProfile.js
+++ react-app/src/components/UserProfiles/CurrentUserProfile.js
@@ -15,16 +15,19 @@ export default function CurrentUserProfile() {
 
     return (
         <div className="user-profiles-wrapper">
-            <div>
+            <div className="user-profiles-header">
                 <div>
-                    <img src={user.profile_image} alt={`User ${user.username} profile image`} />
+                    <img 
+                    className="user-profile-main-image"
+                    src={user.profile_image ? user.profile_image : "https://res.cloudinary.com/djp7wsuit/image/upload/v1688998982/Untitled_design_13_wssosv.png"} 
+                    alt={`User ${user.username} profile image`} />
                 </div>
                 <div>
                     <div>
                         <p>Profile</p>
                         <h1>{user.username}</h1>
                     </div>
-                    <div>
+                    <div className="user-profile-details">
                         <p>{userPlaylistsArr.length} Playlists</p>
                         <i class="fa-solid fa-circle"></i>
                         <p>{userFollowersArr.length === 1 ? "1 Follower" : `${userFollowersArr.length} Followers`}</p>
@@ -35,10 +38,11 @@ export default function CurrentUserProfile() {
             </div>
             <div>
                 <h2>Playlists</h2>
-                <div>
+                <div className="user-profile-playlists-wrapper">
                     {userPlaylistsArr.map(playlist => (
                         <div onClick={() => history.push(`/playlist/${playlist.id}`)}>
                             <img
+                                className="user-profile-playlists-cover-image"
                                 src={playlist.cover_image ? playlist.cover_image : "https://lofy.s3.us-east-2.amazonaws.com/album_covers/Untitled+design+(5).png"}
                                 alt={`Playlist ${playlist.name} cover image`}
                             />
@@ -50,10 +54,11 @@ export default function CurrentUserProfile() {
                 </div>
                 {userFollowersArr.length ? <div>
                     <h2>Followers</h2>
-                    <div>
+                    <div className="user-profile-followers-following-wrapper">
                         {userFollowersArr.map(user => (
                             <div onClick={() => history.push(`/user/${user.username}`)}>
                                 <img
+                                className="other-users-profile-images"
                                     src={user.profile_image ? user.profile_image : "https://res.cloudinary.com/djp7wsuit/image/upload/v1688998982/Untitled_design_13_wssosv.png"}
                                     alt={`User ${user.username} profile image`}
                                 />
@@ -67,10 +72,11 @@ export default function CurrentUserProfile() {
                 </div> : null}
                 {userFollowingArr.length ? <div>
                     <h2>Following</h2>
-                    <div>
+                    <div className="user-profile-followers-following-wrapper">
                         {userFollowingArr.map(user => (
                             <div onClick={() => history.push(`/user/${user.username}`)}>
                                 <img
+                                className="other-users-profile-images"
                                     src={user.profile_image ? user.profile_image : "https://res.cloudinary.com/djp7wsuit/image/upload/v1688998982/Untitled_design_13_wssosv.png"}
                                     alt={`User ${user.username} profile image`}
                                 />
