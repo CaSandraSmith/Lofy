c6e5c371a173968573ea36a4b5264d89b4a3badb
CaSandraSmith <casresm3@gmail.com>
2023-07-13 03:15:03 -0400

    Fixed issue with rendering of reviews page

--- react-app/src/components/ReviewsPage/index.js
+++ react-app/src/components/ReviewsPage/index.js
@@ -13,7 +13,7 @@ export default function ReviewsPage() {
     const [loading, setLoading] = useState(false)
 
     useEffect(() => {
-        dispatch(getCurrentUserReviews()).then(() => setLoading)
+        dispatch(getCurrentUserReviews()).then(() => setLoading(true))
     }, [dispatch])
 
     let user = useSelector(state => state.session.user)
